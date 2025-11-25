80c5f8cbd0f1af4223572ccb405ca7fceecba5f1
hayelom <70315287+hayelom123@users.noreply.github.com>
2025-11-25 11:41:50 +0000

    if statements

--- 101/1.cpp
+++ 101/1.cpp
@@ -3,5 +3,13 @@ using namespace std;
 
 int main(){
   cout<<"Hello GOD!"<<endl;
+
+  cout<<"Please Enter A Holiday Number: ";
+  int holiday;
+  cin>>holiday;
+
+  if(holiday==16){
+    cout<<"Happy Kidanemihret day!"<<endl;
+  }
     return 0;
 }
