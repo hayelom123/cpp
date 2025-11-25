783e0c9be7b16d9509d28d10d6b7f5c1663b9ed1
hayelom <70315287+hayelom123@users.noreply.github.com>
2025-11-25 12:21:14 +0000

    oprators

---
+++ 101/9_oprators.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+
+    // Arithmetic Operators
+    int a = 10, b = 3;
+    cout<<"Arithmetic Operators:" << endl;
+    cout<<"a = "<<a<<endl;
+    cout<<"b = "<<b<<endl;
+    cout << "a + b = " << a + b << endl; // Addition
+    cout << "a - b = " << a - b << endl; // Subtraction
+    cout << "a * b = " << a * b << endl; // Multiplication
+    cout << "a / b = " << a / b << endl; // Division
+    cout << "a % b = " << a % b << endl; // Modulus or reminder
+    cout << "++a = " << ++a << endl;     // Pre-increment
+    cout << "b-- = " << b-- << endl;     // Post-decrement
+    cout << "New value of b = " << b << endl;
+    return 0;
+
+}
