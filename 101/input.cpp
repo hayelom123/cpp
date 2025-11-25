2f9ef608472cb2e96f04f2a61b1748b833c89885
hayelom <70315287+hayelom123@users.noreply.github.com>
2025-11-25 11:54:15 +0000

    input output

---
+++ 101/input.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+
+using namespace std;
+
+int main(){
+    char first_name[30];
+    cout<<"Enter your first name: "<<endl;
+
+    cin>>first_name;
+
+    cout<<"Hello "<<first_name<<", have a nice day!"<<endl;
+    return 0;
+}
---
+++ 101/outpt.cpp
@@ -0,0 +1,10 @@
+#include<iostream>
+using namespace std;
+
+// c++ output statement
+int main(){
+    
+    cout<<"Hello GOD!"<<endl;
+    cout<<"THis is your child seeking redemption!"<<endl;
+    return 0;
+}
