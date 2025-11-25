283b93ad5e7a3105d79aa67e2305c661f177d1da
hayelom <70315287+hayelom123@users.noreply.github.com>
2025-11-25 11:59:08 +0000

    boolean

---
+++ 101/bool.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+
+   // Boolean variables
+   // true = 1, false = 0
+    bool a = true;
+    bool b = false;
+    cout<<"a = "<<a<<endl;
+    cout<<"b = "<<b<<endl;
+
+    return 0;
+}
