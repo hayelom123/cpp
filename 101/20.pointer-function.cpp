5dfa94c3843c2446c3dc4c484f7645de0e7d4d31
Hayelom Kiros <hayelomkiros20@gmail.com>
2026-01-06 20:25:29 +0300

    function pointer with param

--- 101/20.pointer-function.cpp
+++ 101/20.pointer-function.cpp
@@ -4,9 +4,21 @@ void hey()
 {
     std::cout << "Hello from hey function!" << std::endl;
 }
+void helloName(std::string name)
+{
+    std::cout << "Hello: " << name << std::endl;
+}
 int main()
 {
+
     auto funcPtr = hey; // pointer to function hey
     funcPtr();          // call the function using the function pointer
+
+    void (*someFunName)(/*parametrs here*/);
+    someFunName = hey;
+    someFunName();
+
+    void (*helloHayelom)(std::string name) = helloName;
+    helloHayelom("Hayelom Kiros");
     return 0;
 }
