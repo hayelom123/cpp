89a173bd278c53c9c6a111a6e47bdc7c41b8f813
Hayelom Kiros <hayelomkiros20@gmail.com>
2026-01-07 12:13:41 +0300

    including notes(short description)

--- 101/21.lambda.cpp
+++ 101/21.lambda.cpp
@@ -11,12 +11,16 @@ void forEach(std::vector<int> &values, void (*func)(int))
 int main()
 {
     // lambda function
-    //
+    // it was started in c++ 11
+    // Constructs a closure (an unnamed function object capable of capturing variables in scope).
+    //[captures] front-attr(optional) specs except (optional)  back - attr (optional)trailing (optional)contract - specs ﻿(optional){body}
 
-    std::vector<int> values = {1, 5, 6, 8};
+    std::vector<int>
+        values = {1, 5, 6, 8};
 
     auto lambda = [](int value)
     { std::cout << "Value:" << value << std::endl; };
+    //
 
     forEach(values, lambda);
 }
