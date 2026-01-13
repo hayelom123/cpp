53573dc11465f5cabc8850f590d3e50a9d271d06
Hayelom Kiros <hayelomkiros20@gmail.com>
2026-01-13 23:12:22 +0300

    password generated

--- projects/password-generator/main.cpp
+++ projects/password-generator/main.cpp
@@ -1,16 +1,27 @@
 #include <iostream>
+#include <cstdlib> // Required for rand() and srand()
+#include <ctime>   // Required for time()
 
 std::string uppercase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
 std::string lowercase = "abcdefghijklmnopqrstuvwxyz";
 std::string numbers = "0123456789";
 std::string special = "!@#$%^&*()_+-=[]{}|;:,.<>?";
 
-int validatePasswordLength(int length) {}
+int validatePasswordLength(int length)
+{
+    return 0;
+}
 
 std::string possiblePasswords = lowercase + uppercase + numbers + special;
 
 char getRandomCharacter()
 {
+
+    int length = possiblePasswords.length();
+    int randomIndex = std::rand() % length;
+    std::cout << "RandomIndex: " << randomIndex << std::endl;
+
+    return possiblePasswords[randomIndex];
 }
 
 int main()
@@ -21,11 +32,13 @@ int main()
 
     std::cout << "Password Length: " << length << std::endl;
 
-    char *password = "";
+    std::string password = "";
 
     for (int i = 0; i < length; i++)
     {
+        password += getRandomCharacter();
     }
+    std::cout << "Password: " << password << std::endl;
 
     return 0;
 }
