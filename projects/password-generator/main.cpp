dc2a49bb32500e5becb532fdda10e7d554d52039
Hayelom Kiros <hayelomkiros20@gmail.com>
2026-01-13 23:44:58 +0300

    refactor the functions

--- projects/password-generator/main.cpp
+++ projects/password-generator/main.cpp
@@ -1,24 +1,46 @@
 #include <iostream>
 #include <random>
 
-std::string uppercase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-std::string lowercase = "abcdefghijklmnopqrstuvwxyz";
-std::string numbers = "0123456789";
-std::string special = "!@#$%^&*()_+-=[]{}|;:,.<>?";
-
-std::string possiblePasswords = lowercase + uppercase + numbers + special;
-
-char getRandomCharacter()
+int generateRandom(int max)
 {
-    int length = possiblePasswords.length();
-
     std::random_device rd;
     std::mt19937 engine(rd());
-    std::uniform_int_distribution<int> dist(0, length);
+    std::uniform_int_distribution<int> dist(0, max);
 
     int randomIndex = dist(engine);
+    return randomIndex;
+}
 
-    return possiblePasswords[randomIndex];
+std::string generatePassword(int length, bool upper = true, bool lower = true, bool digits = true, bool symbols = true)
+
+{
+    std::cout << "Password Length: " << length << std::endl;
+
+    std::string uppercase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+    std::string lowercase = "abcdefghijklmnopqrstuvwxyz";
+    std::string numbers = "0123456789";
+    std::string special = "!@#$%^&*()_+-=[]{}|;:,.<>?";
+
+    std::string possiblePasswords = "";
+    if (upper)
+        possiblePasswords += uppercase;
+    if (lower)
+        possiblePasswords += lowercase;
+    if (digits)
+        possiblePasswords += numbers;
+    if (symbols)
+        possiblePasswords += symbols;
+
+    int max = possiblePasswords.length();
+
+    std::string password = "";
+
+    for (int i = 0; i < length; i++)
+    {
+        password += possiblePasswords[generateRandom(max)];
+    }
+
+    return password;
 }
 
 int main()
@@ -32,12 +54,8 @@ int main()
 
     // std::cout << "Password Length: " << length << std::endl;
 
-    std::string password = "";
+    std::string password = generatePassword(length);
 
-    for (int i = 0; i < length; i++)
-    {
-        password += getRandomCharacter();
-    }
     std::cout << "Password: " << password << std::endl;
 
     return 0;
