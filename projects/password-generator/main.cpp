2555411f3734a3be5a1e11b1ce22ba0e4531acaa
Hayelom Kiros <hayelomkiros20@gmail.com>
2026-01-13 23:01:53 +0300

    possible passwords

--- projects/password-generator/main.cpp
+++ projects/password-generator/main.cpp
@@ -1,5 +1,18 @@
 #include <iostream>
 
+std::string uppercase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+std::string lowercase = "abcdefghijklmnopqrstuvwxyz";
+std::string numbers = "0123456789";
+std::string special = "!@#$%^&*()_+-=[]{}|;:,.<>?";
+
+int validatePasswordLength(int length) {}
+
+std::string possiblePasswords = lowercase + uppercase + numbers + special;
+
+char getRandomCharacter()
+{
+}
+
 int main()
 {
     int length;
@@ -7,6 +20,12 @@ int main()
     std::cin >> length;
 
     std::cout << "Password Length: " << length << std::endl;
+
+    char *password = "";
+
+    for (int i = 0; i < length; i++)
+    {
+    }
 
     return 0;
 }
