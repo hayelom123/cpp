41510e1429f3a91ebab018eecfe51df2451b8fa9
Hayelom Kiros <hayelomkiros20@gmail.com>
2026-01-13 22:52:56 +0300

    password generator

--- 101/13_inheritance.cpp
+++ 101/13_inheritance.cpp
@@ -4,84 +4,100 @@
 using namespace std;
 
 // Base class
-class User{
+class User
+{
 public:
     string username;
     string email;
 
-    void displayInfo() {
+    void displayInfo()
+    {
         cout << "Username: " << username << ", Email: " << email << endl;
     }
 };
 
 // Derived class
-class Admin :public User{
+class Admin : public User
+{
 public:
     int adminLevel;
 
-    void displayAdminInfo() {
+    void displayAdminInfo()
+    {
         displayInfo();
         cout << "Admin Level: " << adminLevel << endl;
     }
 };
 
 // multi-level inheritance example
-class SuperAdmin : public Admin {
-    public:
-        void displaySuperAdminInfo() {
-            displayAdminInfo();
-            cout <<username <<" is Super Admin." << endl;
-        }
+class SuperAdmin : public Admin
+{
+public:
+    void displaySuperAdminInfo()
+    {
+        displayAdminInfo();
+        cout << username << " is Super Admin." << endl;
+    }
 };
 
-class Guest : public User {
+class Guest : public User
+{
 public:
-    void displayGuestInfo() {
+    void displayGuestInfo()
+    {
         displayInfo();
         cout << username << " is a Guest User." << endl;
     }
 };
 
 // multiple inheritance example
-class Moderator{
+class Moderator
+{
 public:
     string stage;
-    void displayModeratorInfo() {
-       
+    void displayModeratorInfo()
+    {
+
         cout << stage << " is a Moderator." << endl;
     }
 };
 
-class VIPUser : public User, public Moderator {
-    
-    public:
-        void displayVIPInfo() {
-            User::displayInfo();
-            Moderator::displayModeratorInfo();
-            cout << username << " is a VIP User." << endl;
-        }
+class VIPUser : public User, public Moderator
+{
+
+public:
+    void displayVIPInfo()
+    {
+        User::displayInfo();
+        Moderator::displayModeratorInfo();
+        cout << username << " is a VIP User." << endl;
+    }
 };
 
 // virtual inheritance example can be added here if needed
 
-class Animal{
+class Animal
+{
 public:
-   virtual void sound() {
+    virtual void sound()
+    {
         cout << "Animal sounds" << endl;
     }
-
 };
 
-class Dog:public Animal{
+class Dog : public Animal
+{
 public:
-    void sound() override {
+    void sound() override
+    {
         cout << "Dog barks" << endl;
     }
 };
 
-int main() {
+int main()
+{
 
-    cout<<"-------------------------------------------"<<endl;
+    cout << "-------------------------------------------" << endl;
 
     // Creating an object of the base class
     User user1;
@@ -89,17 +105,16 @@ int main() {
     user1.email = "jond@gmail.com";
     user1.displayInfo();
 
-    cout<<"-------------------------------------------"<<endl;
+    cout << "-------------------------------------------" << endl;
 
     // Creating an object of the derived class
     Admin admin1;
-    admin1.username = "admin_user"; 
+    admin1.username = "admin_user";
     admin1.email = "ad@gmail.com";
     admin1.adminLevel = 5;
     admin1.displayAdminInfo();
 
-    cout<<"-------------------------------------------"<<endl;
-
+    cout << "-------------------------------------------" << endl;
 
     // Creating an object of the multi-level derived class
     SuperAdmin superAdmin1;
@@ -108,28 +123,31 @@ int main() {
     superAdmin1.adminLevel = 10;
     superAdmin1.displaySuperAdminInfo();
 
-    cout<<"-------------------------------------------"<<endl;
+    cout << "-------------------------------------------" << endl;
 
-     // creating multiple inheritance object
+    // creating multiple inheritance object
     VIPUser vip1;
     vip1.username = "vip_user";
     vip1.email = "mod@gmail.com";
     vip1.stage = "Gold";
     vip1.displayVIPInfo();
 
+    cout << "------------- ------------------------------" << endl;
+    // creating virtual inheritance object
 
-     cout<<"-------------------------------------------"<<endl;
-// creating virtual inheritance object
-
-  Animal* a;
+    Animal *a;
     Dog d;
-    a=&d;
-    a->sound();// without virtual it would have called Animal sound function not the child
+    a = &d;
+    a->sound(); // without virtual it would have called Animal sound function not the child
 
-      cout<<"-------------------------------------------"<<endl;
+    cout << "-------------------------------------------" << endl;
 
-    //clearing output screen
-    cout<<endl<<endl<<endl<<endl<<endl;
+    // clearing output screen
+    cout << endl
+         << endl
+         << endl
+         << endl
+         << endl;
 
     return 0;
 }
---
+++ projects/password-generator/main.cpp
@@ -0,0 +1,7 @@
+#include <iostream>
+
+int main()
+{
+    std::cout << "Password genarator!!!\n";
+    return 0;
+}
